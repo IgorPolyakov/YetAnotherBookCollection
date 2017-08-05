287f0c6c9586d9f988df9b403c9272a6af0c0587
IgorPolyakov <igorpolyakov@protonmail.com>
2017-08-05 15:59:08 +0700

    some fix

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,6 +1,7 @@
 class Book < ApplicationRecord
   belongs_to :user
   belongs_to :category
+  has_many :reviews
   has_attached_file :book_cover, styles: { cover: '300x300>', preview: '100x100>' }, default_url: '/images/:style/missing.png'
   validates_attachment_content_type :book_cover, content_type: /\Aimage\/.*\z/
  end
