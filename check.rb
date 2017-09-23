5fa9e5e056308557ab63dd2b61fc3859a673d2a3
IgorPolyakov <igorpolyakov@protonmail.com>
2017-09-23 18:25:22 +0700

    update

--- check.rb
+++ check.rb
@@ -19,40 +19,7 @@ def check
   end
 end
 
-def new_put
-  uri_q = URI('http://localhost:3000/signup')
-  http = Net::HTTP.new(uri_q.host, uri_q.port)
-  path = '/signup'
-  email = 'email'
-  password = 'password'
-  request = Net::HTTP::Get.new uri.path
-  response = http.request request
-  resp, data = http.get(path, nil)
-  # response = http.request request
-  token = response.body.split('<meta name="csrf-token" content="')[1].split('"')[0]
-  cookie = response.get_fields('set-cookie')[1].split(';')[0]
-
-  headers = {
-    'Content-Type': 'application/x-www-form-urlencoded',
-    'Cookie': cookie
-  }
-
-  aray = {
-    'utf8': '✓',
-    'authenticity_token':  token,
-    'user[email]':  email,
-    'user[password]':  password,
-    'commit':  'Submit'
-  }
-  resp, data = http.post(path, URI.encode_www_form(aray), headers)
-
-  puts 'Code = ' + resp.code
-  puts 'Message = ' + resp.message
-  resp.each { |key, val| puts key + ' = ' + val }
-  puts data
-end
-
-def newnewput(email, password)
+def register_user(email, password)
   uri = URI 'http://localhost:3000/signup'
   http = Net::HTTP.new uri.host, uri.port
   request = Net::HTTP::Get.new uri.path
@@ -60,21 +27,39 @@ def newnewput(email, password)
 
   token = response.body.split('<meta name="csrf-token" content="')[1].split('"')[0]
 
-  cookie =  cookie = response['set-cookie'].split(';')[0]
+  cookie = response['set-cookie'].split(';')[0]
 
   form_data = {
     'authenticity_token': token,
     'user[email]':  email,
     'user[password]':  password,
-    'commit':  'Submit'
+    'commit': 'Submit'
   }
 
-  uri = URI 'http://localhost:3000/signup'
+  uri = URI 'http://localhost:3000/users'
   http = Net::HTTP.new uri.host, uri.port
-  request = Net::HTTP::Post.new uri.path, 'COOKIE' => cookie
+  headers = {
+    'Referer': 'http://localhost:3000/signup',
+    'Content-Type': 'application/x-www-form-urlencoded',
+    'Host': 'localhost:3000',
+    'Cookie': cookie
+  }
+  request = Net::HTTP::Post.new uri.path, headers
   request.set_form_data form_data
   response = http.request request
 
   p response.body
 end
-newnewput("#{(rand * 1000000_.to_i}", '1')
+
+def login_user
+uri = URI 'http://localhost:3000/login'
+http = Net::HTTP.new uri.host, uri.port
+request = Net::HTTP::Get.new uri.path
+response = http.request request
+
+token = response.body.split('<meta name="csrf-token" content="')[1].split('"')[0]
+
+cookie = response['set-cookie'].split(';')[0]
+end
+
+register_user((rand * 1_000_000).to_i.to_s, '1')
