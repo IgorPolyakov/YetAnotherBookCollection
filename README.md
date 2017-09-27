41dc25fe4369f874a3b2d0175f5e479f953cdda5
IgorPolyakov <igorpolyakov@protonmail.com>
2017-09-27 21:30:23 +0700

    get in checker - work

--- README.md
+++ README.md
@@ -11,13 +11,19 @@
 ToDo:
 
 - [ ] Bugs
-  - [ ] DOS - to_sym. Symbol — это особый тип данных в Ruby, являющийся, по сути, константой, при этом сами символы никогда не удаляются сборщиком мусора. Соответственно можно забить память пользовательским вводом.
+  - [x] DOS - to_sym. Symbol — это особый тип данных в Ruby, являющийся, по сути, константой, при этом сами символы никогда не удаляются сборщиком мусора. Соответственно можно забить память пользовательским вводом.
 
 - [ ] Docker
 
 - [ ] Juri
 
+Dependency:
 
+- Rails
+
+- ImageMagic
+
+- PostgreSQL
 
 [Explotation]: https://habrahabr.ru/company/pentestit/blog/326272/
 [Mass Assignment]: https://xakep.ru/2013/12/03/ruby-on-rails-safe-problem/#toc06.
--- checker.py
+++ checker.py
@@ -4,10 +4,8 @@
 from xvfbwrapper import Xvfb
 from selenium import webdriver
 from time import sleep
-import random
-
-def get(arg):
-    pass
+from faker import Faker
+fake = Faker()
 
 def check(ui_addr):
     vdisplay = Xvfb()
@@ -22,43 +20,54 @@ def check(ui_addr):
         return 101
     else:
         return 104
-def put(ui_addr, ui_email, ui_passwd):
+
+def put(ui_addr, ui_email, ui_passwd, flag):
     vdisplay = Xvfb()
     vdisplay.start()
     chrome_options = webdriver.ChromeOptions()
     chrome_options.add_argument('--no-sandbox')
     browser = webdriver.Chrome(chrome_options=chrome_options)
     browser.get('http://{}:3000/signup'.format(ui_addr))
+
     browser.set_window_size(1920, 1080)
 
-    browser.find_element_by_xpath('//*[@id="user_name"]').send_keys("ui_name")
+    browser.find_element_by_xpath('//*[@id="user_name"]').send_keys(fake.name())
     browser.find_element_by_xpath('//*[@id="user_email"]').send_keys(ui_email)
     browser.find_element_by_xpath('//*[@id="user_password"]').send_keys(ui_passwd)
     browser.find_element_by_xpath('/html/body/div/div/div[2]/form/input[3]').click()
-
-    sleep(1)
-
-    # browser.get('http://{}:3000/books/new'.format(ui_addr))
     browser.find_element_by_xpath('/html/body/nav /div/ul[2]/li[2]').click()
 
     browser.get_screenshot_as_file('/tmp/book_new.png')
 
-    sleep(1)
-
-    browser.find_element_by_xpath('//*[@id="book_title"]').send_keys("ui_name")
-    browser.find_element_by_xpath('//*[@id="book_desc"]').send_keys("ui_name")
-    browser.find_element_by_xpath('//*[@id="book_author"]').send_keys("ui_name")
-    # browser.find_element_by_xpath('//*[@id="book_category_id"]').selectByVisibleText('Other')?
-    # browser.find_element_by_xpath('//*[@id="book_category_id"]/option[1]').click()
+    browser.find_element_by_xpath('//*[@id="book_title"]').send_keys(fake.catch_phrase())
+    browser.find_element_by_xpath('//*[@id="book_desc"]').send_keys("sibirctf:" + flag)
+    browser.find_element_by_xpath('//*[@id="book_author"]').send_keys(fake.name_male())
     browser.find_element_by_xpath('/html/body/div/form/div[2]/select/option[3]').click()
-    # browser.findelement(by.xpath('//*[@id="book_category_id"]/option[1]')).click
-    # browser.findElement({ xpath: '//*[@id="book_category_id"]/div/div[1]/select' }).sendKeys(Mr.);
-    # /html/body/div/form/div[2]/select/option[3]
-    # By.xpath("//select[@id='groupSelect']/option[1]")
     browser.find_element_by_xpath('//*[@id="book_book_cover"]').send_keys("/home/fox/Desktop/gen473_2654653.jpg")
     browser.find_element_by_xpath('//*[@id="book_is_privat"]').click()
     browser.find_element_by_xpath('/html/body/div/form/input[3]').click()
 
-    browser.get_screenshot_as_file('/tmp/scr.png')
+    browser.get_screenshot_as_file('/tmp/put.png')
+
+def get(ui_addr, ui_email, ui_passwd):
+    vdisplay = Xvfb()
+    vdisplay.start()
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.add_argument('--no-sandbox')
+    browser = webdriver.Chrome(chrome_options=chrome_options)
+    browser.get('http://{}:3000/login'.format(ui_addr))
+    browser.set_window_size(1920, 1080)
+    browser.find_element_by_xpath('//*[@id="email"]').send_keys(ui_email)
+    browser.find_element_by_xpath('//*[@id="password"]').send_keys(ui_passwd)
+    browser.find_element_by_xpath('/html/body/div/div/div[2]/form/div[3]/input').click()
+    browser.find_element_by_xpath('/html/body/div/div[1]/div/a').click()
+    browser.get_screenshot_as_file('/tmp/get.png')
+    flag = browser.find_element_by_xpath('/html/body/div/div[1]/div[1]/div/p').text
+    print(flag)
+
 # check('localhost')
-put('localhost', str(random.random() * 100) + '@maoi.com', 'ui_passwd')
+ui_passwd = fake.password(length=10, special_chars=True, digits=True, upper_case=True, lower_case=True)
+ui_email = fake.email()
+flag = fake.credit_card_number(card_type=None)
+put('localhost', ui_email, ui_passwd, flag)
+get('localhost', ui_email, ui_passwd)
