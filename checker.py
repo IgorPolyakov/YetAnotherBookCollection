563d9f2e38a0a9525d2905e868da80ae60a20b3a
Igor Polyakov <igor.polyakov@elecard.ru>
2017-09-29 09:59:39 +0700

    try implement try catch

--- checker.py
+++ checker.py
@@ -4,6 +4,7 @@
 from sys import argv
 from xvfbwrapper import Xvfb
 from selenium import webdriver
+from selenium.common.exceptions import NoSuchElementException
 from time import sleep
 from faker import Faker
 import hashlib
@@ -40,23 +41,26 @@ def put(argv):
 
     browser.set_window_size(1920, 1080)
 
-    browser.find_element_by_xpath('//*[@id="user_name"]').send_keys(fake.name())
-    browser.find_element_by_xpath('//*[@id="user_email"]').send_keys(ui_email)
-    browser.find_element_by_xpath('//*[@id="user_password"]').send_keys(ui_passwd)
-    browser.find_element_by_xpath('/html/body/div/div/div[2]/form/input[3]').click()
-    browser.find_element_by_xpath('/html/body/nav /div/ul[2]/li[2]').click()
-
-    browser.get_screenshot_as_file('/tmp/book_new.png')
-
-    browser.find_element_by_xpath('//*[@id="book_title"]').send_keys(fake.catch_phrase())
-    browser.find_element_by_xpath('//*[@id="book_desc"]').send_keys("sibirctf:" + flag)
-    browser.find_element_by_xpath('//*[@id="book_author"]').send_keys(fake.name_male())
-    browser.find_element_by_xpath('/html/body/div/form/div[2]/select/option[3]').click()
-    browser.find_element_by_xpath('//*[@id="book_book_cover"]').send_keys("/home/fox/Desktop/gen473_2654653.jpg")
-    browser.find_element_by_xpath('//*[@id="book_is_privat"]').click()
-    browser.find_element_by_xpath('/html/body/div/form/input[3]').click()
-
-    browser.get_screenshot_as_file('/tmp/put.png')
+    try:
+        browser.find_element_by_xpath('//*[@id="user_name"]').send_keys(fake.name())
+        browser.find_element_by_xpath('//*[@id="user_email"]').send_keys(ui_email)
+        browser.find_element_by_xpath('//*[@id="user_password"]').send_keys(ui_passwd)
+        browser.find_element_by_xpath('/html/body/div/div/div[2]/form/input[3]').click()
+        browser.find_element_by_xpath('/html/body/nav /div/ul[2]/li[2]').click()
+    except NoSuchElementException:
+        return (CORRUPT)
+    try:
+        browser.find_element_by_xpath('//*[@id="book_title"]').send_keys(fake.catch_phrase())
+        browser.find_element_by_xpath('//*[@id="book_desc"]').send_keys("sibirctf:" + flag)
+        browser.find_element_by_xpath('//*[@id="book_author"]').send_keys(fake.name_male())
+        browser.find_element_by_xpath('/html/body/div/form/div[2]/select/option[3]').click()
+        browser.find_element_by_xpath('//*[@id="book_book_cover"]').send_keys("/home/fox/Desktop/gen473_2654653.jpg")
+        browser.find_element_by_xpath('//*[@id="book_is_privat"]').click()
+        browser.find_element_by_xpath('/html/body/div/form/input[3]').click()
+    except NoSuchElementException:
+        return (CORRUPT)
+    return (OK)
+    # browser.get_screenshot_as_file('/tmp/put.png')
 
 # def get(ui_addr, ui_email, ui_passwd):
 def get(argv):
